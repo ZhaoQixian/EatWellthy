adf6ec234b634bf877ec0d592c342cb59aa98802
ZhaoQixian <142140884+ZhaoQixian@users.noreply.github.com>
2024-09-29 22:13:04 +0800

    Code Upload 1st time
    Added the fundamental structure,  read ReadMeQixian to learn how to use

---
+++ client/src/FAQs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FAQs = () => {
+  return (
+    <div>
+      <h1>FAQs</h1>
+      <p>Welcome to the FAQs page!</p>
+    </div>
+  );
+};
+
+export default FAQs;
---
+++ client/src/Welloh.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welloh = () => {
+  return (
+    <div>
+      <h1>Welloh</h1>
+      <p>Welcome to the Welloh page!</p>
+    </div>
+  );
+};
+
+export default Welloh;
