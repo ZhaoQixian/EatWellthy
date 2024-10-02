71608e1ed373b4b17da2605b21f681d5728423ee
rishikamehta2004@gmail.com <rishikamehta2004@gmail.com>
2024-10-02 10:42:39 +0800

    update authentication and profile

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const auth = require("./routes/Auth");
+const favicon = require("serve-favicon");
+const path = require("path"); // Ensure path is required
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+// Body parser middleware (using express.json and express.urlencoded directly)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// CORS options
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your frontend URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable cookies to be sent in cross-origin requests
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Use CORS with options
+app.use(cors(corsOptions));
+
+// Basic route
+app.use("/users", auth);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("mern-auth/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "mern-auth", "build", "index.html"));
+  });
+}
+
+// Start the server
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux"; // Make sure you import Provider
+import { thunk } from "redux-thunk"; // Correctly import thunk as a named export
+import rootReducer from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const initialState = {};
+
+const middleware = [thunk];
+
+// Create the Redux store
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+// Render the application
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {" "}
+      {/* Wrap your App with Provider */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance in your app
 reportWebVitals();
+
+export default store; // Export the store for use in other files
