0067ac2db345d1f2494aab9677bf3da1d6736056
ZhaoQixian <142140884+ZhaoQixian@users.noreply.github.com>
2024-10-11 11:40:57 +0800

    link dailypricelist
    link daily price list to mongodb, create supermarkets database in mongodb atlas.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import supermarkets from './supermarkets';
+
+
 export default combineReducers({
   alert,
   auth,
+  supermarkets,
 });
