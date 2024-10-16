db6c2ec7ac7513cd6db8241ee6c13748aa56cd8c
nicoleelow <nicolelow0712@gmail.com>
2024-10-16 10:14:35 +0800

    Return google user information from backend

--- backend/routes/googleAuth.js
+++ backend/routes/googleAuth.js
@@ -15,15 +15,29 @@ googleAuth.get(
 
 googleAuth.get("/success", (req, res) => {
   if (req.user) {
+    // console.log("Google INFO", req.user);
+    // console.log("ID : ", req.user.id);
+    // console.log("Name : ", "Google User (" + req.user.emails[0].value + ")");
+    // console.log("Email : ", req.user.emails[0].value);
+    // console.log("Avatar : ", req.user.photos[0].value);
+    // console.log("Date : ", null);
+
     const payload = {
       user: {
         id: req.user.id,
       },
     };
+    const user = {
+      id: req.user.id,
+      name: "Google User (" + req.user.emails[0].value + ")",
+      email: req.user.emails[0].value,
+      avatar: req.user.photos[0].value || null,
+      date: null,
+    };
 
     jwt.sign(payload, jwtSecret, { expiresIn: "5 days" }, (err, token) => {
       if (err) throw err;
-      return res.status(200).json({ success: true, token, user: req.user.id });
+      return res.status(200).json({ success: true, token, user });
     });
   } else return res.status(400).json({ error: "no Google log in" });
 });
