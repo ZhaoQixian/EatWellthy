514976a4e511eecfca08f821ab2dd161e33bcad0
CatilonyZhang <zyc2068521267@gmail.com>
2024-10-22 23:01:17 +0800

    Fix: Superarket get api fixed

--- backend/routes/wellohAI.js
+++ backend/routes/wellohAI.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+router.get("/chat",async(req,res)=>{
+    
+})
+
--- client/src/actions/supermarkets.js
+++ client/src/actions/supermarkets.js
@@ -4,7 +4,7 @@ import { GET_SUPERMARKETS, SUPERMARKETS_ERROR } from './types';
 // Fetch supermarket brands
 export const getSupermarkets = () => async (dispatch) => {
   try {
-    const res = await axios.get('http://localhost:5050/api/supermarkets');  // Use the full URL with port 5050
+    const res = await axios.get('http://localhost:5050/api/supermarkets/supermarket_data');  // Use the full URL with port 5050
     dispatch({
       type: GET_SUPERMARKETS,
       payload: res.data,  // Dispatch the supermarket data
