c56eb2ed312e4830a43e3f8274859e47572055ad
ZhaoQixian <142140884+ZhaoQixian@users.noreply.github.com>
2024-11-09 23:04:37 +0800

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 // client/src/config.js
 const config = {
     backendUrl: process.env.NODE_ENV === 'production'
-      ? 'https://eatwellthy-backend.com'  // your Render backend URL
+      ? 'https://eatwellthy-backend.onrender.com'  // Changed to Render URL
       : 'http://localhost:5050',
     defaultErrorMsg: 'An error occurred. Please try again.'
   };
